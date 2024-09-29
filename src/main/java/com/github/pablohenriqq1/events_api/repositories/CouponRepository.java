24eae8640d9741997e4e977c9826efdbd6a7e1f2
PabloH <pablohenriqq01@gmail.com>
2024-09-29 18:45:05 -0300

    Adicionando repository da classe/entidade Coupon.

--- src/main/java/com/github/pablohenriqq1/events_api/repositories/CouponRepository.java
+++ src/main/java/com/github/pablohenriqq1/events_api/repositories/CouponRepository.java
@@ -1,11 +1,11 @@
 package com.github.pablohenriqq1.events_api.repositories;
 
-import com.github.pablohenriqq1.events_api.models.Event;
+import com.github.pablohenriqq1.events_api.models.Coupon;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.UUID;
 
 @Repository
-public interface EventRepository extends JpaRepository<Event, UUID> {
+public interface CouponRepository extends JpaRepository<Coupon, UUID> {
 }
